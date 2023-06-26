4eb95d17fa5f5f4d89278876b8128c734f4f3c02
gonza <alvarezgonzalo0022@gmail.com>
2023-06-26 10:58:59 -0300

    Finished Anagrams, chunk and fizzbuzz

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,64 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+  
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+      return false;
+    }
+  
+    for (let char in aCharMap) {
+      if (aCharMap[char] !== bCharMap[char]) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  const buildCharMap = (str) => {
+    const charMap = {};
+  
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+      charMap[char] = charMap[char] + 1 || 1;
+    }
+  
+    return charMap;
+  }
 
 module.exports = anagrams;
+
+// const string1 = stringA.replace(/[^\w]/g, "").toLowerCase();
+// const string2 = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+// if(string1.length !== string2.length) return false;
+
+// let letters1 = {};
+// let letters2 = {};
+
+// for (const char of string1) {
+//     letters1[char] = letters1[char] + 1 || 1;
+// }
+// for (const char of string2) {
+//     letters2[char] = letters2[char] + 1 || 1;
+// }
+
+// for (const char of Object.keys(letters1)) {
+//     if(!letters2[char] || letters1[char] !== letters2[char]) return false
+// }
+// return true;
+
+// function anagrams(stringA, stringB) {
+//     return cleanString(stringA) === cleanString(stringB);
+//   }
+  
+//   function cleanString(str) {
+//     return str
+//       .replace(/[^\w]/g, '')
+//       .toLowerCase()
+//       .split('')
+//       .sort()
+//       .join('');
+//   }
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,41 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+      chunked.push(array.slice(index, index + size));
+      index += size;
+    }
+
+    return chunked;
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   const chunked = [];
+//
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+//
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//
+//   return chunked;
+// }
+
+// let output = [];
+    
+// for(let i = 0; i < array.length; i){
+//     output.push(array.splice(i, size));
+// }
+
+// return output;
+
