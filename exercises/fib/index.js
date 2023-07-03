2ceb5cba0276f74eda42d610a10c6107fbf5e1df
gonza <alvarezgonzalo0022@gmail.com>
2023-07-03 19:03:54 -0300

    queue and weave

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,40 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if(cache[args]) return cache[args];
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    }
+}
+
+function slowFib(n) {
+    if(n < 2) return n;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
+
+// const results = [0 , 1];
+
+// for(let i = 2; i <= n; i++) {
+//     const a = results[i - 1];
+//     const b = results[i - 2];
+//     results.push(a + b);
+// }
+
+// return results[n];
+
+// -----------------------------------
+
+// if(n < 2) return n;
+
+// return fib(n - 1) + fib(n - 2);
