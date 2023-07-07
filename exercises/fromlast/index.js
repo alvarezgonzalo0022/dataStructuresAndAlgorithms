8933143d75cd05dd555cdb92dc42bb27d9f0b99b
zalo0022 <alvarezgonzalo0022@gmail.com>
2023-07-07 16:51:44 -0300

    Added circular, fromlast, midpoint, tree andlevelw

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,20 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    for(let i = 0; i < n; i++) {
+        fast = fast.next;
+    }
+
+    while(fast.next) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    return slow;
+}
 
 module.exports = fromLast;
--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, "S"];
+    const counter = [0];
+
+    while(arr.length > 1) {
+        const node = arr.shift();
+
+        if (node === "S") {
+            counter.push(0);
+            arr.push("S");
+          } 
+          else {
+            arr.push(...node.children);
+            counter[counter.length - 1]++;
+          }
+    }
+
+    return counter;
+}
 
 module.exports = levelWidth;
