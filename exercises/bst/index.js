87d69dd8a9159af173b5919b40b9640b483e0078
zalo0022 <alvarezgonzalo0022@gmail.com>
2023-07-12 22:07:53 -0300

    Added bst, events, sorting, validate

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,38 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.right = null;
+        this.left = null;
+    }
+    
+    insert(data) {
+        if(data < this.data && this.left) {
+            this.left.insert(data);
+        }
+        else if(data < this.data) {
+            this.left = new Node(data);
+        }
+        else if(data > this.data && this.right) {
+            this.right.insert(data);
+        }
+        else if(data > this.data) {
+            this.right = new Node(data);
+        }
+    }
+
+    contains(data) {
+        if (this.data === data) return this;
+
+        if (data < this.data) {
+            return this.left ? this.left.contains(data) : null;
+        } 
+        else {
+            return this.right ? this.right.contains(data) : null;
+        }
+    }
+}
 
 module.exports = Node;
--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,34 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.data = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if(this.data[eventName]){
+      this.data[eventName].push(callback);
+    }
+    else {
+      this.data[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if(this.data[eventName]) {
+      for (const cb of this.data[eventName]) {
+        cb();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.data[eventName];
+  }
 }
 
 module.exports = Events;
